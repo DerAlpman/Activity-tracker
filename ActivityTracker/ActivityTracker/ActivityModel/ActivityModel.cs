1fc5aaddfa0a8f28af620da525a6c555b44a7f40
Stefan Alpers <stefan.alpers@gmx.net>
2019-01-12 22:59:44 +0100

    Added ActivityModel

---
+++ ActivityTracker/ActivityTracker/ActivityModel/ActivityModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ActivityTracker.ActivityModel
+{
+    internal class ActivityModel
+    {
+        #region FIELDS
+
+        private DateTime _TimeStamp;
+        private string _Text;
+
+        #endregion
+
+        public ActivityModel(DateTime timeStamp, string text)
+        {
+            this._TimeStamp = timeStamp;
+            this._Text = text;
+        }
+
+        #region PROPERTIES
+
+        public string Text { get => _Text; set => _Text = value; }
+        public DateTime TimeStamp { get => _TimeStamp; set => _TimeStamp = value; }
+
+        #endregion
+    }
+}
