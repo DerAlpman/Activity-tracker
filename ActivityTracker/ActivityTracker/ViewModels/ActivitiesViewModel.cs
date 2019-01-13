09c48a193a6438d0c1693a6bc428b4c0ed6438b8
Stefan Alpers <stefan.alpers@gmx.net>
2019-01-13 19:41:55 +0100

    * Dummy data for ActivitiesViewModel * XAML cleanup * DataGrid additions

--- ActivityTracker/ActivityTracker/ViewModels/ActivitiesViewModel.cs
+++ ActivityTracker/ActivityTracker/ViewModels/ActivitiesViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using ActivityTracker.Models;
 using Components.ActivityTracker.Interfaces;
 
 namespace ActivityTracker.ViewModels
@@ -11,9 +13,27 @@ namespace ActivityTracker.ViewModels
 
         #endregion
 
+        public ActivitiesViewModel()
+        {
+            _Activities = new ObservableCollection<IActivityModel>();
+            LoadActivities();
+        }
+
         #region PROPERTIES
 
         public ObservableCollection<IActivityModel> Activities { get => _Activities; set => _Activities = value; }
+
+        #endregion
+
+        #region METHODS
+
+        private void LoadActivities()
+        {
+            _Activities.Add(new ActivityModel(new DateTime(2019, 1, 22, 8, 0, 0), "Issue 5461"));
+            _Activities.Add(new ActivityModel(new DateTime(2019, 1, 22, 8, 20, 0), "Issue 5461"));
+            _Activities.Add(new ActivityModel(new DateTime(2019, 1, 22, 8, 40, 0), "Issue 5461"));
+            _Activities.Add(new ActivityModel(new DateTime(2019, 1, 22, 9, 00, 0), "Sprintretrospektive"));
+        }
 
         #endregion
     }
