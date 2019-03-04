667079245e0f7e5fee378c321cf11efcabcc54ea
Stefan Alpers <stefan.alpers@gmx.net>
2019-03-04 22:09:26 +0100

    ActivityInputControlsViewModel

---
+++ ActivityTracker/Library.ActivityTracker/ViewModels/ActivityInputControlsViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Library.ActivityTracker.ViewModels
+{
+    public class ActivityInputControlsViewModel
+    {
+        #region FIELDS
+
+        private string _Text;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public string Text { get => _Text; set => _Text = value; }
+
+
+        #endregion
+    }
+}
