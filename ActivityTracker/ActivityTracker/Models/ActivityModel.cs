1eb58aee55a08b4f93ed95d3f4066171ba390e58
Stefan Alpers <stefan.alpers@gmx.net>
2019-05-20 18:58:25 +0000

    Revert "[Refs #10] added combo box item"
    This reverts commit 9216b4234df8b3834a04b9b2a754580cbd8191b9

--- ActivityTracker/ActivityTracker/Models/ActivityModel.cs
+++ ActivityTracker/ActivityTracker/Models/ActivityModel.cs
@@ -7,16 +7,14 @@ namespace ActivityTracker.Models
     {
         #region FIELDS
 
-        private readonly int _OrderNumber;
         private readonly DateTime _TimeStamp;
         private readonly string _Text;
-        private readonly TimeSpan _TimeSpan;
+        private TimeSpan _TimeSpan;
 
         #endregion
 
-        public ActivityModel(int orderNumber, DateTime timeStamp, string text, TimeSpan timeSpan)
+        public ActivityModel(DateTime timeStamp, string text, TimeSpan timeSpan)
         {
-            this._OrderNumber = orderNumber;
             this._TimeStamp = timeStamp;
             this._Text = text;
             this._TimeSpan = timeSpan;
@@ -24,10 +22,9 @@ namespace ActivityTracker.Models
 
         #region PROPERTIES
 
-        public int OrderNumber => _OrderNumber;
-        public string Text => _Text;
-        public DateTime TimeStamp => _TimeStamp;
-        public TimeSpan TimeSpan => _TimeSpan;
+        public string Text { get => _Text; }
+        public DateTime TimeStamp { get => _TimeStamp; }
+        public TimeSpan TimeSpan { get => _TimeSpan; }
 
         #endregion
     }
